d24dd70330369e55128095a65d02f261dad8f4c8
Shevelev Viacheslav <shevslawa@gmail.com>
2020-02-16 14:23:51 +0300

    Little changes

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -3,77 +3,75 @@
 #include "time.h"
 
 void swap(int *a, int *b) {
-	int c = *a;
-	*a = *b;
-	*b = c;
+        int c = *a;
+        *a = *b;
+        *b = c;
 }
 
 void quicksort(int *array, int len) {
-	if (len < 2) {
-		return;
-	}
-	else {
-		int pivot = array[0];
-		int less_len = 0;
-		int greater_len = 0;
-		for (int i = 1; i < len; i++) {
-			if (array[i] <= pivot) {
-				swap(&array[less_len], &array[i]);
-				less_len++;
-				swap(&array[less_len], &array[i]);
-			}
-			else {
-				greater_len++;
-			}
-		}
-		quicksort(array, less_len);
-		quicksort(&array[less_len + 1], greater_len);
-		return;
-	}
-				
+        if (len < 2) {
+                return;
+        }
+        else {
+                int pivot = array[0];
+                int less_len = 0;
+                int greater_len = 0;
+                for (int i = 1; i < len; i++) {
+                if (array[i] <= pivot) {
+                                swap(&array[less_len], &array[i]);
+                                less_len++;
+                                swap(&array[less_len], &array[i]);
+                        }
+                        else {
+                                greater_len++;
+                        }
+                }
+                quicksort(array, less_len);
+                quicksort(&array[less_len + 1], greater_len);
+                return;
+        }
+
 }
 
 void array_random_value(int *array, int length) {
-	srand(time(NULL));
-	for (int i = 0; i < length; i++) {
-		array[i] = rand() % 100000;
-	}
-	return;
+        srand(time(NULL));
+        for (int i = 0; i < length; i++) {
+                array[i] = rand() % 100000;
+        }
+        return;
 }
 
 //That function print array to stdout
 void print_array(int *array, int length) {
-	printf("[");
-	for (int i = 0; i < length; i++) {
-		printf("%i ", array[i]);
-	}
-	printf("]");
+        printf("[");
+        for (int i = 0; i < length; i++) {
+                printf("%i ", array[i]);
+        }
+        printf("]");
 }
 
 int main(int argc, char *argv) {
-	int length;
-	//offer to user to choose length of array
-	printf("Input length of array ");
-	scanf("%i", &length);
-	//give memory to array
-	int *array = malloc(length * sizeof(int));
-	//pass random values to array
-	array_random_value(array, length);
-	printf("\nArray initial: ");
-	print_array(array, length);
-	quicksort(array, length);
-	printf("\nArray ascending: ");
-	print_array(array, length);
-	int check = 0;
-	for (int i = 0; i < length - 1; i++) {
-		if (array[i + 1] < array[i]) {
-			check = 1;
-		}
-	}
-	if (check == 0) {
-		printf("\nTest passed");
-	}
-	return 0;
+        int length;
+        //offer to user to choose length of array
+        printf("Input length of array ");
+        scanf("%i", &length);
+        //give memory to array
+        int *array = malloc(length * sizeof(int));
+        //pass random values to array
+        array_random_value(array, length);
+        printf("\nArray initial: ");
+        print_array(array, length);
+        quicksort(array, length);
+        printf("\nArray ascending: ");
+        print_array(array, length);
+        int check = 0;
+        for (int i = 0; i < length - 1; i++) {
+                if (array[i + 1] < array[i]) {
+                        check = 1;
+                }
+        }
+        if (check == 0) {
+                printf("\nTest passed");
+        }
+        return 0;
 }
-
-
--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -1,3 +1,5 @@
+import random
+
 def quicksort(list):
     if len(list) < 2:
         return list
@@ -13,5 +15,10 @@ def quicksort(list):
 
         return quicksort(less) + [pivot] + quicksort(greater)
 
-array = [1, 6, 3, 8, 33, 1, 4, 2, 854, 3, 4, 44]
-print(quicksort(array))
+list = []
+length = int(input("Input length of array "))
+for counter in range(0, length):
+    list.append(random.randint(0, 100000))
+sorted_array = quicksort(list)
+#print("Initial array is " + str(list))
+#print("Sorted array is " + str(sorted_array))
